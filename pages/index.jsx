91840408d928dd54d8fc285cf6e067bdbacbc4a5
Ricky352 <lopes.ricky97@gmail.com>
2022-12-04 02:03:22 +0100

    First commit

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import { AppBar, Toolbar } from '@mui/material'
+import ResponsiveAppBar from '../components/ResponsiveAppBar'
+
+export default function Home() {
+  return (
+    <>
+      <ResponsiveAppBar />
+    </>
+  )
+}
