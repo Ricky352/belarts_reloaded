69ba916b8371267937c274cf72394811612affb9
Ricky352 <lopes.ricky97@gmail.com>
2022-12-04 02:30:49 +0100

    Add ImageList

---
+++ components/TitlebarBelowImageList.jsx
@@ -0,0 +1,110 @@
+import * as React from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import { Container } from "@mui/system";
+import { Card } from "@mui/material";
+import { Tooltip } from "@mui/material";
+import { Avatar } from "@mui/material";
+import { Rating } from "@mui/material";
+import { StarBorder } from "@mui/icons-material";
+
+export default function TitlebarBelowImageList() {
+  return (
+    <Container>
+      <ImageList
+        gap={12}
+        sx={{
+          mb: 8,
+          gridTemplateColumns:
+            "repeat(auto-fill, minmax(280px, 1fr))!important",
+        }}
+      >
+        {itemData.map((item) => (
+          <Card key={item.id}>
+            <ImageListItem sx={{ height: "100% !important" }}>
+              <ImageListItemBar
+                sx={{
+                  background:
+                    "linear-gradient(to bottom, rgba(0,0,0,0.7)0%, rgba(0,0,0,0.3)70%, rgba(0,0,0,0)100%)",
+                }}
+                title={item.title === 0 ? "Free Stay" : "$" + item.title}
+                actionIcon={
+                  <Tooltip title={item.author} sx={{ mr: "5px" }}>
+                    <Avatar src={item.author} />
+                  </Tooltip>
+                }
+                position="top"
+              />
+              <img
+                src={item.img}
+                alt={item.title}
+                loading="lazy"
+                style={{ cursor: "pointer" }}
+              />
+              <ImageListItemBar
+                title={item.author}
+                actionIcon={
+                  <Rating
+                    sx={{ color: "rgba(255,255,255, 0.8)", mr: "5px" }}
+                    name="room-rating"
+                    defaultValue={3.5}
+                    precision={0.5}
+                    emptyIcon={
+                      <StarBorder sx={{ color: "rgba(255,255,255, 0.8)" }} />
+                    }
+                  />
+                }
+              />
+            </ImageListItem>
+          </Card>
+        ))}
+      </ImageList>
+    </Container>
+  );
+}
+
+const itemData = [
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/commons/e/e4/Latte_and_dark_coffee.jpg",
+    title: "Breakfast",
+    author: "@bkristastucchio",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+    author: "@rollelflex_graphy726",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+    author: "@helloimnik",
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+    author: "@nolanissac",
+  },
+  {
+    id: 5,
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+    author: "@hjrc33",
+  },
+  {
+    id: 6,
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+    author: "@arwinneil",
+  },
+  {
+    id: 7,
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+    author: "@tjdragotta",
+  },
+];
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import { AppBar, Toolbar } from '@mui/material'
 import ResponsiveAppBar from '../components/ResponsiveAppBar'
+import TitlebarBelowImageList from '../components/TitlebarBelowImageList'
 
 export default function Home() {
   return (
     <>
       <ResponsiveAppBar />
+      <TitlebarBelowImageList />
     </>
   )
 }
