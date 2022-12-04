775fb37a00aaed6e162872aa5d2106ff709139ac
Ricky352 <lopes.ricky97@gmail.com>
2022-12-04 16:05:42 +0100

    Add Routes

---
+++ pages/_app.jsx
@@ -0,0 +1,29 @@
+import "../styles/globals.css";
+
+import { ThemeProvider } from "@mui/material/styles";
+import { purple, red } from "@mui/material/colors";
+import { createTheme, useTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#615e5b",
+    },
+    secondary :{
+      main: red[500],
+
+    }
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
